9f5b63a9986c55f9feaa9e2a171f03d9a8f072f9
DevRizel <rbracero27@gmail.com>
2024-07-20 00:24:49 +0800

    newdatabase1

--- setting/db.php
+++ setting/db.php
@@ -3,7 +3,7 @@
 define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'u510162695_inventory');
 define('DB_PASS', '1Inventory_system');
-define('DB_NAME', 'u510162695_inventory');
+define('DB_NAME', '');
 define('DB_PORT', '3306'); // Note: Changed to DB_PORT to align with typical naming conventions
 
 // Attempt to connect to the database
