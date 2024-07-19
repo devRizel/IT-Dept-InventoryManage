d27f3661f4006fd87fc855b52db59d0f41a61047
DevRizel <rbracero27@gmail.com>
2024-07-20 00:04:17 +0800

    forgetpass

--- setting/db.php
+++ setting/db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-	$db = new PDO('mysql:host=127.0.0.1;dbname=u510162695_inventory','u510162695_inventory','1Inventory_system');
+	$db = new PDO('mysql:host=localhost;dbname=inventory_system','root','');
 	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
 	die('<h4 style="color:red">Incorrect Connection Details</h4>');
