4afadb6dc1f07cdf1fd6c3a1540186b4def723a0
DevRizel <rbracero27@gmail.com>
2024-07-20 15:35:33 +0800

    new

--- includes/config.php
+++ includes/config.php
@@ -15,7 +15,7 @@
   define( 'DB_HOST', '127.0.0.1' );          // Set database host
   define( 'DB_USER', 'u510162695_inventory' );             // Set database user
   define( 'DB_PASS', '1Inventory_system' );             // Set database password
-  define( 'DB_NAME', '' );        // Set database name
+  define( 'DB_NAME', 'u510162695_inventory' );        // Set database name
   define( 'Port', '3306');
 
 ?>
