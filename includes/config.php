5749d68dc2000181eac0498eebf67f8915aeb844
DevRizel <rbracero27@gmail.com>
2024-07-22 12:02:12 +0800

    connect

--- includes/config.php
+++ includes/config.php
@@ -1,21 +1,9 @@
 <?php
-/*
-|--------------------------------------------------------------------------
-| OWSA-INV V2
-|--------------------------------------------------------------------------
-| Author: Siamon Hasan
-| Project Name: OSWA-INV
-| Version: v2
-| Offcial page: http://oswapp.com/
-| facebook Page: https://www.facebook.com/oswapp
-|
-|
-|
-*/
-  define( 'DB_HOST', '127.0.0.1' );          // Set database host
-  define( 'DB_USER', 'u510162695_inventory' );             // Set database user
-  define( 'DB_PASS', '1Inventory_system' );             // Set database password
-  define( 'DB_NAME', '' );        // Set database name
+
+  define( 'DB_HOST', '127.0.0.1' );        
+  define( 'DB_USER', 'u510162695_inventory' );       
+  define( 'DB_PASS', '1Inventory_system' );  
+  define( 'DB_NAME', 'u510162695_inventory' );  
   define( 'Port', '3306');
 
 ?>
