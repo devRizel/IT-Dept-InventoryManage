3b0da6d90f31ae1aa4a79e9bef17e7e4681cf1ef
DevRizel <rbracero27@gmail.com>
2024-08-10 09:46:22 -0700

    iloveyourosemarie

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<link rel="icon" type="image/x-icon" href="uploads/users/rizel.png">
 <?php
 if (!isset($_GET['access']) || $_GET['access'] !== 'allowed') {
     // Redirect to index.php if the access parameter is not set correctly
