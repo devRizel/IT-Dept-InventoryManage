2aeec2d614307c1acc625dcc21ae19dad7807a6d
DevRizel <rbracero27@gmail.com>
2024-09-13 21:11:22 +0800

    yes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ date_default_timezone_set('Asia/Manila');
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Inventory Management System</title>
+  <title>Inventory Management Systems</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
