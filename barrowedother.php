621ca479120000336359ed6ee40bbeaa6adbab88
DevRizel <rbracero27@gmail.com>
2024-09-15 18:06:45 +0800

    yes

--- barrowedother.php
+++ barrowedother.php
@@ -10,34 +10,37 @@ $filtered_other = [];
 
 
 function fetch_other() {
-    global $db;
-  
-    $sql = "SELECT 
-              p.id, 
-              p.name, 
-              p.categorie_id, 
-              p.donate, 
-              p.dreceived, 
-              p.media_id, 
-              p.date,
-              p.serial, 
-              c.name AS categorie, 
-              m.file_name AS image,
-              p.other_images
-            FROM 
-              other p 
-            LEFT JOIN 
-              categories c ON c.id = p.categorie_id 
-            LEFT JOIN 
-              media m ON m.id = p.media_id 
-            WHERE 
-              p.other_images NOT LIKE '%Maintenance%' 
-              AND p.barrow NOT LIKE '%Return%' 
-            ORDER BY 
-              p.id ASC";
-  
-    return find_by_sql($sql);
+  global $db;
+
+  $sql = "SELECT 
+            p.id, 
+            p.name, 
+            p.categorie_id, 
+            p.donate, 
+            p.dreceived, 
+            p.media_id, 
+            p.date,
+            p.serial, 
+            c.name AS categorie, 
+            m.file_name AS image,
+            p.other_images
+          FROM 
+            other p 
+          LEFT JOIN 
+            categories c ON c.id = p.categorie_id 
+          LEFT JOIN 
+            media m ON m.id = p.media_id 
+          WHERE 
+            p.other_images NOT LIKE '%Maintenance%' 
+            AND p.barrow NOT LIKE '%Return%' 
+            AND p.barrow IS NOT NULL 
+            AND p.barrow != '' 
+          ORDER BY 
+            p.id ASC";
+
+  return find_by_sql($sql);
 }
+
 
 
   
