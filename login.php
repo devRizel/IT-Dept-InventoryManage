1f862a06756416a658b38c3c8e756d55c21ebd2f
DevRizel <rbracero27@gmail.com>
2024-09-24 23:28:25 +0800

    yes

--- login.php
+++ login.php
@@ -7,8 +7,12 @@ if (!isset($_GET['access']) || $_GET['access'] !== 'allowed') {
 }
 ?>
 
-<?php include 'setting/system.php'; ?>
-<?php include 'theme/head.php'; ?>
+<?php
+date_default_timezone_set('Asia/Manila');
+  ob_start();
+  // require_once('includes/load.php');
+  if($session->isUserLoggedIn(true)) { redirect('home.php', false);}
+?>
 <?php include_once('layouts/header.php'); ?>
 <div class="login-page-wrapper">
   <div class="login-page">
