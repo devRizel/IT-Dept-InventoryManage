78205ffa93e9bc534a3af452026b3e67b15c4703
DevRizel <rbracero27@gmail.com>
2024-09-28 19:15:18 +0800

    yes

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
   define( 'DB_HOST', '127.0.0.1' );        
   define( 'DB_USER', 'u510162695_inventory' );       
   define( 'DB_PASS', '1Inventory_system' );  
-  define( 'DB_NAME', '' );  
+  define( 'DB_NAME', 'u510162695_inventory' );  
   define( 'Port', '3306');
 
 ?>
