0a56cc2e6cfe208ed506621bb5e16c553e29c20f
DevRizel <rbracero27@gmail.com>
2024-10-11 23:05:41 +0800

    yes

--- login_v2.php
+++ login_v2.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('Asia/Manila');
   ob_start();
-  // require_once('includes/load.php');
+  require_once('includes/load.php');
   if($session->isUserLoggedIn(true)) { redirect('home.php', false);}
 ?>
 
