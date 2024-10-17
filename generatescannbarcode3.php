13cc4cce891578d227dc8bd48961f6d6a434a2ec
DevRizel <rbracero27@gmail.com>
2024-10-17 17:29:43 +0800

    yes

--- generatescannbarcode3.php
+++ generatescannbarcode3.php
@@ -131,14 +131,14 @@ include('admin/db_connect.php');
 <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav" style="background-color: var(--accent-color);">
     <div class="container">
         <a class="iska"></a>
-        <a class="navbar-brand js-scroll-trigger" href="index.php" >INVENTORY MANAGEMENT SYSTEM</a>
+        <a class="navbar-brand js-scroll-trigger" href="index.php" style="color: white;">INVENTORY MANAGEMENT SYSTEM</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto my-2 my-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="L-Login.php?access=allowed" style="font-size: 20px; ">Login Now</a>
+                    <a class="nav-link js-scroll-trigger" href="L-Login.php?access=allowed" style="font-size: 20px; color: white;">Login Now</a>
                 </li>
             </ul>
         </div>
