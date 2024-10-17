a64b3735cc85acbcdf9307a53c3d351abbddd681
DevRizel <rbracero27@gmail.com>
2024-10-17 11:01:19 +0800

    ty

--- generate.php
+++ generate.php
@@ -54,7 +54,7 @@ include('admin/db_connect.php');
 <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav" style="background-color: var(--accent-color);">
     <div class="container">
         <a class="iska"></a>
-        <a class="navbar-brand js-scroll-trigger" href="index.php" >INVENTORY MANAGEMENT SYSTEM</a>
+        <a class="navbar-brand js-scroll-trigger" href="index.php" >INVENTORY MANAGEMENT SYSTEMss</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
