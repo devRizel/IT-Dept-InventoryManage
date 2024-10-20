7789feb4b88689b15a4e190f5a01e8de2fc2d0c6
DevRizel <rbracero27@gmail.com>
2024-10-20 18:12:35 +0800

    yes

--- index.php
+++ index.php
@@ -456,7 +456,7 @@ background: linear-gradient(90deg, rgba(203,204,203,1) 0%, rgba(147,149,150,1) 9
       <div class="container">
         <div class="row gy-4">
           <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
-            <center><h1>INVENTORY MANAGEMENTsssss</h1></center>
+            <center><h1>INVENTORY MANAGEMENT</h1></center>
             <center><h1>SYSTEM</h1></center>
             <br><br>
             <center><p>Please Select Portal to proceed.</p></center>
